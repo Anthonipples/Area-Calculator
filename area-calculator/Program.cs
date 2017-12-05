b48cb652fd822cc559acf99a268b52a095466b6c
Anthony Lombard <anthony.isalive@gmail.com>
2017-12-05 10:50:43 +0200

    Initial commit

---
+++ area-calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the width of your object in Meters");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the height of your object in Meters");
+            double height = double.Parse(Console.ReadLine());
+            double area = width * height;
+            Console.WriteLine("The area of your object is: " + area + " Meters Squared");
+            Console.ReadLine();
+
+        }
+    }
+}
